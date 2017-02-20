8023891c0e1c78561018d86c2f841cd76c10bd89
greymd <greengregson@gmail.com>
2017-02-20 11:03:52 -0500

    Fix bug

--- docker-zsh-completion.plugin.zsh
+++ docker-zsh-completion.plugin.zsh
@@ -1,5 +1,5 @@
 # For docker
-fpath+="`dirname $0`/docker/docker/master/contrib/completion/zsh/_docker"
+fpath+="`dirname $0`/docker/docker/master/contrib/completion/zsh/"
 
 # For docker-compose
 fpath+="`dirname $0`/docker/compose/master/contrib/completion/zsh/"
