fe664367e3a88f94862ea82d0c724e8d0be8b05d
greymd <greengregson@gmail.com>
2017-05-25 10:08:05 +0900

    Add execute.sh

--- execute.sh
+++ execute.sh
@@ -1,26 +1,26 @@
 #!/bin/bash -x
-readonly CONTENT_PATH="$HOME/reps/greymd/docker-zsh-completion"
+readonly THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%N}}")"; pwd)"
 readonly BRANCH_NAME="master"
 
-cd "$CONTENT_PATH"
+cd "$THIS_DIR"
 git config --local user.name greymd
 git config --local user.email greengregson@gmail.com
 
 : "Get docker/docker" && {
     _path="docker/docker/master/contrib/completion/zsh/_docker"
-    mkdir -p "$CONTENT_PATH/$(dirname ${_path})"
-    curl -L "https://raw.githubusercontent.com/$_path" > "$CONTENT_PATH/$_path"
+    mkdir -p "$THIS_DIR/$(dirname ${_path})"
+    curl -L "https://raw.githubusercontent.com/$_path" > "$THIS_DIR/$_path"
 }
 
 : "Get docker/compose" && {
     _path="docker/compose/master/contrib/completion/zsh/_docker-compose"
-    mkdir -p "$CONTENT_PATH/$(dirname ${_path})"
-    curl -L "https://raw.githubusercontent.com/$_path" > "$CONTENT_PATH/$_path"
+    mkdir -p "$THIS_DIR/$(dirname ${_path})"
+    curl -L "https://raw.githubusercontent.com/$_path" > "$THIS_DIR/$_path"
 }
 
 git pull origin master
 
-sed -i -r 's/^(LAST UPDATE:).*$/\1'$(date +%F)'/' $CONTENT_PATH/README.md
+sed -i -r 's/^(LAST UPDATE:).*$/\1'$(date +%F)'/' $THIS_DIR/README.md
 
 git add -A
 git commit -m "$(date +%Y%m%d)_auto_commit"
