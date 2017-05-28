8484596ed6aa358b0032213b62204d616063e989
greymd <greengregson@gmail.com>
2017-05-28 16:25:43 +0900

    Refactoring

--- execute.sh
+++ execute.sh
@@ -1,32 +1,34 @@
 #!/bin/bash -x
 readonly THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%N}}")"; pwd)"
+readonly REPO_DIR="$THIS_DIR/repos"
 readonly BRANCH_NAME="master"
 
 cd "$THIS_DIR"
+rm -rf "${REPO_DIR}/*"
 git config --local user.name greymd
 git config --local user.email greengregson@gmail.com
 
 : "Get moby/moby" && {
     _path="moby/moby/master/contrib/completion/zsh/_docker"
-    mkdir -p "$THIS_DIR/$(dirname ${_path})"
-    curl -L "https://raw.githubusercontent.com/$_path" > "$THIS_DIR/$_path"
+    mkdir -p "$REPO_DIR/$(dirname ${_path})"
+    curl -L "https://raw.githubusercontent.com/$_path" > "$REPO_DIR/$_path"
 }
 
 : "Get docker/compose" && {
     _path="docker/compose/master/contrib/completion/zsh/_docker-compose"
-    mkdir -p "$THIS_DIR/$(dirname ${_path})"
-    curl -L "https://raw.githubusercontent.com/$_path" > "$THIS_DIR/$_path"
+    mkdir -p "$REPO_DIR/$(dirname ${_path})"
+    curl -L "https://raw.githubusercontent.com/$_path" > "$REPO_DIR/$_path"
 }
 
 git pull origin master
 
 sed -i -r 's/^(LAST UPDATE:).*$/\1'$(date +%F)'/' $THIS_DIR/README.md
 
-git add -A
-git commit -m "$(date +%Y%m%d)_auto_commit"
-if [ ${?} -eq 0 ]; then
-  # If the commit is succeeded, push it to github.
-  git push origin $BRANCH_NAME
-fi
+# git add -A
+# git commit -m "$(date +%Y%m%d)_auto_commit"
+# if [ ${?} -eq 0 ]; then
+#   # If the commit is succeeded, push it to github.
+#   git push origin $BRANCH_NAME
+# fi
 
 exit 0
